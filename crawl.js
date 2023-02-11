84ab8a0e541b06847fbfe71b4d5070b9e5dfaadb
Irfan Dayan <engr.irfandayan@gmail.com>
2023-02-11 12:53:30 +0500

    getURLsFromHTML function & its tests

--- crawl.js
+++ crawl.js
@@ -1,13 +1,42 @@
+const { JSDOM } = require("jsdom");
+
 function normalizeURL(urlString) {
   const urlObj = new URL(urlString);
-
   const hostPath = `${urlObj.hostname}${urlObj.pathname}`;
-
   if (hostPath.length > 0 && hostPath.slice(-1) === "/") {
     return hostPath.slice(0, -1);
   }
   return hostPath;
 }
+
+function getURLsFromHTML(htmlBody, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll("a");
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0, 1) === "/") {
+      // relative
+      try {
+        const urlObj = new URL(`${baseURL}${linkElement.href}`);
+        urls.push(urlObj.href);
+      } catch (error) {
+        console.log(`error with relative url: ${error.message}`);
+      }
+    } else {
+      //absolute
+
+      try {
+        const urlObj = new URL(linkElement.href);
+        urls.push(urlObj.href);
+      } catch (error) {
+        console.log(`error with absolute url: ${error.message}`);
+      }
+    }
+  }
+  return urls;
+}
+
 module.exports = {
   normalizeURL,
+  getURLsFromHTML,
 };
